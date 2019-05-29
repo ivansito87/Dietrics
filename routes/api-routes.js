d0ce07878f3261f0b99803773690aa5af25d126d
Ivan Rendon <ivansito87@gmail.com>
2019-05-29 14:05:44 -0400

    adds routes for app

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,6 @@
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
 var path = require("path");
-var foods = require("foods.json");
 
 // ===============================================================================
 // Routing
@@ -12,29 +11,13 @@ var foods = require("foods.json");
 
 module.exports = function (app) {
 
-    // Respond with the html file for home
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-
-    //Respond with the html for survey
-    app.get("/user", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/user.html"))
-    });
-
-    //Respond with the html for survey
-    app.get("/pt", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/pt.html"))
-    });
+    const foods = require("../scripts/foods.json");
 
     //Respond with the api call
     app.get("/api/foods", function (req, res) {
         return res.json(foods);
     });
 
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
 
 };
 
