39d1ebb29f41e3a0fd743aeb5380b41dede9987a
Uncle Sam <djiasara@gmail.com>
2019-05-31 14:00:12 -0400

    create foods models

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,3 +1,6 @@
 //"large" 1.2x calories, "small" .8x calories to standard one "   " [food]
 //cup, teaspoon, tablespoon, etc. conversions table needed
 //total weight is grams
+
+const foods = require('./foods');
+console.log(foods);
